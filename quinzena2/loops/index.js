243f36ed1702cb58ec32c07553978b1bbb4ee542
marcilioQueiroz <queiroz.marcilio@gmail.com>
2021-06-10 22:58:32 -0300

    Correcao do exercicio (tinha esquecido de fazer em funcoes)

--- quinzena2/loops/index.js
+++ quinzena2/loops/index.js
@@ -38,52 +38,71 @@ if(qtdBixosEstimacao === 0){
 
 //2)
 
-//let arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
 
   //a)
-/*    for(let numero of arrayOriginal){
-    console.log(numero)
-  } 
- */
+
+/*  / function imprimirNumerosArray(){
+    for(let numero of arrayOriginal){
+      console.log(numero)
+    } 
+  }
+  imprimirNumerosArray()  */
 
   //b) 
-/*   for(let indice = 0; indice < arrayOriginal.length; indice++){
+/* function imprimirNumerosArrayDiv10(){
+  for(let indice = 0; indice < arrayOriginal.length; indice++){
     let valorIndiceArrayDivPor10 = arrayOriginal[indice] / 10
     console.log(valorIndiceArrayDivPor10) 
   }
- */
+}
+imprimirNumerosArrayDiv10() */
 
   //c)
-/*   let listaNumerosPares = []
-  for(let numero of arrayOriginal){
-     if(numero % 2 === 0){
-      listaNumerosPares.push(numero)
-    }
-  } 
-  console.log(listaNumerosPares) */
   
+  /* function novaListaNumerosPares(arrayNumeros){
+    let listaNumerosPares = []
+    for(let numero of arrayNumeros){
+       if(numero % 2 === 0){
+        listaNumerosPares.push(numero)
+      }
+    } 
+  }
+  
+  console.log(listaNumerosPares) 
+   */
   // d)
-/*   let listaString = []
+
+ /*  function novoArray(){
+    let listaString = []
   for(let index = 0; index < arrayOriginal.length; index++){
     let frasePorIndice = `O elemento do índex ${index} é: ${arrayOriginal[index]}`
     listaString.push(frasePorIndice)
   } 
-  console.log(listaString)
- */
+  return listaString
+  }
+  let novaListaaa = novoArray()
+  console.log(novaListaaa) */
 
   //e)
-/* let maiorNumero = 0
-let menorNumero = 999999999999999999999999
-for(let numero of arrayOriginal){
-  if(numero > maiorNumero){
-    maiorNumero = numero
-  }
 
-  if(numero < menorNumero){
-    menorNumero = numero
+function verificarMaiorEMenor(){
+  let maiorNumero = 0
+  let menorNumero = 999999999999999999999999
+  let mensagen = ""
+  for(let numero of arrayOriginal){
+    if(numero > maiorNumero){
+      maiorNumero = numero
+    }
+  
+    if(numero < menorNumero){
+      menorNumero = numero
+    }
   }
+  return mensagen = `O maior numero: ${maiorNumero} e o menor numero: ${menorNumero}`
 }
-console.log(`O maior numero: ${maiorNumero} e o menor numero: ${menorNumero}`)
- */
+let resposta = verificarMaiorEMenor()
+console.log(resposta)
+
 
 
